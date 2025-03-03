b04369bb37272ae55ffa20bc5b8b463400964921
eliasnijs <elias.nijs@gmail.com>
2025-03-03 21:41:32 +0100

    feat: optimize get quadrant function via bithsifts, changed location of origin point to a more intuitive definition

--- src/utils/octtree.ts
+++ src/utils/octtree.ts
@@ -9,18 +9,18 @@ interface vec3 {
 }
 
 enum Octant {
-	OCTANT_FTL,
-	OCTANT_FTR,
-	OCTANT_FBL,
-	OCTANT_FBR,
-	OCTANT_BTL,
-	OCTANT_BTR,
 	OCTANT_BBL,
 	OCTANT_BBR,
+	OCTANT_BTL,
+	OCTANT_BTR,
+	OCTANT_FBL,
+	OCTANT_FBR,
+	OCTANT_FTL,
+	OCTANT_FTR,
 }
 const OCTANT_STRING_TABLE: string[] = [
-    "Front-Top-Left", "Front-Top-Right", "Front-Bottom-Left", "Front-Bottom-Right",
-	"Back-Top-Left",  "Back-Top-Right",  "Back-Bottom-Left",  "Back-Bottom-Right"
+    "Back-Bottom-Left", "Back-Bottom-Right", "Back-Top-Left", "Back-Top-Right",
+    "Front-Bottom-Left", "Front-Bottom-Right", "Front-Top-Left", "Front-Top-Right"
 ];
 
 enum CTU_State {
@@ -35,7 +35,7 @@ interface CTU_LeafData {
 }
 
 interface CTU {
-	origin:		vec3 // front left top
+	origin:		vec3 // most left,bottom,back point
 	size:		number
 	state:		CTU_State
 	octants:	[CTU, CTU, CTU, CTU, CTU, CTU, CTU, CTU] | null
@@ -59,26 +59,18 @@ interface OctTree {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Utility Functions
 
-const octantMap = [
-        [[Octant.OCTANT_BBL, Octant.OCTANT_BBR],
-		 [Octant.OCTANT_BTL, Octant.OCTANT_BTR]],
-        [[Octant.OCTANT_FBL, Octant.OCTANT_FBR],
-         [Octant.OCTANT_FTL, Octant.OCTANT_FTR]]
-    ]
-
 function getOctant(origin: vec3, size: number, position: vec3): Octant {
 	const halfsize = size / 2
     const centerX = origin.x + halfsize
     const centerY = origin.y + halfsize
     const centerZ = origin.z + halfsize
 
+    const isRight = position.x >= centerX;  // +X is right
+    const isTop = position.y >= centerY; // +Y is top
     const isFront = position.z >= centerZ; // +Z is front
-    const isTop   = position.y >= centerY; // +Y is top
-    const isLeft  = position.x < centerX;  // -X is left
 
-    return octantMap[isFront][isTop][isLeft];
+	return (isFront << 2) | (isTop << 1) | isRight
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Implementation
@@ -132,7 +124,7 @@ insert(tree: OctTree, n: CTU, element: OctreeElement): void {
 						origin: {
 							x: n.origin.x + (octant & 1 ? halfSize : 0),
 							y: n.origin.y + (octant & 2 ? halfSize : 0),
-							z: n.origin.z + (!(octant & 4) ? halfSize : 0)
+							z: n.origin.z + (octant & 4 ? halfSize : 0)
 						},
 						size: halfSize,
 						state: CTU_State.CTU_LEAF,
