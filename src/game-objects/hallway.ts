94e0414d6882be26a1623d4e715cd073580bfb97
renevds <rene.v.d.s.9@gmail.com>
2025-05-02 21:44:34 +0200

    :sparkles: Spawn doors

--- src/game-objects/hallway.ts
+++ src/game-objects/hallway.ts
@@ -80,7 +80,7 @@ export class Hallway extends GameObject {
 			this.rotation = openSides.indexOf(true) * -90
 		} else if (count === 2) {
 			if (north && south) {
-				if (Math.random() < 1 / 1) {
+				if (Math.random() < 1 / 2) {
 					this.type = 'Hall_Door_Large'
 				} else {
 					this.type = getRandomItem(['Hall_Light', 'Hall_NoLight'])
@@ -88,7 +88,7 @@ export class Hallway extends GameObject {
 
 				this.rotation = 0
 			} else if (east && west) {
-				if (Math.random() < 1 / 1) {
+				if (Math.random() < 1 / 2) {
 					this.type = 'Hall_Door_Large'
 				} else {
 					this.type = getRandomItem(['Hall_Light', 'Hall_NoLight'])
